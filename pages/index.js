ab3b0701932a3010a49b96c068c277d6aebc7cdc
Thais <thaisfaria@cvc.ext.com>
2020-04-23 17:38:30 -0300

    photo route

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Base from '../components/base';
-import Carousel from '../components/carousel';
+import { useContext, useEffect } from 'react';
+import Router from 'next/router'
+import getUsers from '../services/getUsers';
+import { UserContext, SelectedUserContext } from '../context/Store';
 
 const Index = () => {
-  return (
-    <>
-      <Base>
-        <Carousel />
-      </Base>
-    </>
-  );
+  const [users, setUsers] = useContext(UserContext);
+  const [selectedUser, setSelectedUser] = useContext(SelectedUserContext);
+
+  useEffect(() => {
+    if (!users) {
+      getUsers().then((data) => {
+        setUsers(data);
+        setSelectedUser(data[0].id);
+        Router.push(`/photos/${data[0].id}`);
+      });
+    }
+  }, []);
+
+  if (selectedUser) Router.push(`/photos/${selectedUser}`);
+
+  return null;
 };
 
 export default Index;
---
+++ pages/photos/[id].js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Base from '../../components/base';
+import Carousel from '../../components/carousel';
+
+const Index = () => {
+  return (
+    <>
+      <Base>
+        <Carousel />
+      </Base>
+    </>
+  );
+};
+
+export default Index;
