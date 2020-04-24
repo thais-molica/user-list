9fbcd8b0df52bb1e871114581c04395a2e1c6bdb
Thais <thaisfaria@cvc.ext.com>
2020-04-24 11:15:41 -0300

    menu

--- components/menu/styles.js
+++ components/menu/styles.js
@@ -2,10 +2,21 @@ import css from 'styled-jsx/css';
 import theme from '../theme';
 
 export default css`
+  nav {
+    margin-bottom: ${theme.spacing(2)};
+  }
   ul {
     display: flex;
   }
   li + li {
-    margin-left: ${theme.spacing(1)};
+    margin-left: ${theme.spacing(2)};
+  }
+  a {
+    font-size: ${theme.font.size.subtitle};
+    font-weight: bold;
+    transition: all ease 0.5s;
+  }
+  a:hover {
+    opacity: 0.7;
   }
 `;
